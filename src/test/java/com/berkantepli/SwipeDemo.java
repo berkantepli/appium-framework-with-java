4b6469ed041310f8a418ea0f56299f9089d37031
Berk Antepli <berkantepli@gmail.com>
2026-09-14 19:46:29 +0300

    Rebase demo 1

--- src/test/java/com/berkantepli/SwipeDemo.java
+++ src/test/java/com/berkantepli/SwipeDemo.java
@@ -32,5 +32,9 @@ public class SwipeDemo extends BaseTest {
     // feature login branch
     // stash demo
     // Pull Request demo
+<<<<<<< HEAD
     // Main Commit
+=======
+    // Rebase Demo 1
+>>>>>>> 22622d3 (Rebase demo 1)
 }
