6b551b2d75fed3aac36318790297aebc2b7f3ac4
Berk Antepli <berkantepli@gmail.com>
2026-09-16 13:42:34 +0300

    Cherry Pick 2

--- src/test/java/com/berkantepli/SwipeDemo.java
+++ src/test/java/com/berkantepli/SwipeDemo.java
@@ -38,4 +38,9 @@ public class SwipeDemo extends BaseTest {
     // Interactive 1
     // Interactive 2
     // Interactive 3
+<<<<<<< HEAD
+=======
+    // Cherry Pick 1
+    // Cherry Pick 2
+>>>>>>> 63134a2 (Cherry Pick 2)
 }
